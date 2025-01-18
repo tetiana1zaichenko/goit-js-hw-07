9510f0e7bf93750a0d05a0a53c7d9d3df41c5178
Tetiana_Zaichenko <tanja.zaychenko@gmail.com>
2025-01-18 13:01:02 +0200

    add task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,17 @@
-
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", event => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.elements.email.value.trim();
+    const password = form.elements.password.value.trim();
+    if (email === "" || password === "") {
+    alert("All form fields must be filled in");
+    return;
+    };
+    const result = {
+        email: email,
+        password: password,
+    };
+    console.log(result);
+     form.reset(); 
+});
