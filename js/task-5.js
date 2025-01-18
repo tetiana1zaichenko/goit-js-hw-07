7df2744a5a07ca5e88483671a77f807a4555672e
Tetiana_Zaichenko <tanja.zaychenko@gmail.com>
2025-01-18 13:30:02 +0200

    add task-5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,11 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btnChangeColor = document.querySelector(".change-color");
+const spanColor = document.querySelector(".color");
+btnChangeColor.addEventListener("click", () => {
+  const color = getRandomHexColor();
+  document.body.style.backgroundColor = color;
+  spanColor.textContent = color;
+  });
